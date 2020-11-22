020432cff20aee002e0939225ba0e2fcea06b2a1
saleheen.noor <saleheen.noor@gmail.com>
2020-11-22 19:05:10 +0500

    Deploying Olx App Clone

---
+++ precache-manifest.0e0e3db9949e3530795c5e886c7321c4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dab7568e54877379b0535922576cb2f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d77272fe1ab4f845f5f",
+    "url": "/static/css/2.ada51479.chunk.css"
+  },
+  {
+    "revision": "fa054e7b83ffc7dde169",
+    "url": "/static/css/main.2d1e0972.chunk.css"
+  },
+  {
+    "revision": "3d77272fe1ab4f845f5f",
+    "url": "/static/js/2.eea22fc3.chunk.js"
+  },
+  {
+    "revision": "9f8702cb55408414e24afc5ac73fd822",
+    "url": "/static/js/2.eea22fc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa054e7b83ffc7dde169",
+    "url": "/static/js/main.d1be1d82.chunk.js"
+  },
+  {
+    "revision": "0b64e02b2873ae065dc4",
+    "url": "/static/js/runtime-main.6b73a7c6.js"
+  },
+  {
+    "revision": "7975761176487dc62e25536d9a36a61d",
+    "url": "/static/media/avatar.79757611.png"
+  },
+  {
+    "revision": "8be1775f4788250b94e09df79f3d464f",
+    "url": "/static/media/emptyChat.8be1775f.webp"
+  },
+  {
+    "revision": "638181100538123713772ceb95a45777",
+    "url": "/static/media/hero_bg_pk.63818110.jpg"
+  },
+  {
+    "revision": "bf119a34de303ced214e0490ebd3f21e",
+    "url": "/static/media/keyboard_backspace-black-18dp.bf119a34.svg"
+  },
+  {
+    "revision": "5d87588fef9d21913aa97962a39efab5",
+    "url": "/static/media/olx_logo.5d87588f.png"
+  },
+  {
+    "revision": "4cb8cbc478714518f696fe164d5f8157",
+    "url": "/static/media/phone-app.4cb8cbc4.png"
+  },
+  {
+    "revision": "6e626917184bb742bcd50adbf979d41a",
+    "url": "/static/media/sell_button.6e626917.png"
+  },
+  {
+    "revision": "ab938d4ddd82d9454d2c80eb243e2e6a",
+    "url": "/static/media/staticmap.ab938d4d.png"
+  }
+]);
